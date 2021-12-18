219a7a605e58474de8293f848d2e6b2bfb90cb0e
RozinaVasileva <vasileva.rozina@gmail.com>
2021-12-18 18:22:11 +0200

    add TourTopViewModel

---
+++ src/main/java/bg/softuni/yacht/model/view/TourTopViewModel.java
@@ -0,0 +1,62 @@
+package bg.softuni.yacht.model.view;
+
+import bg.softuni.yacht.model.entity.DestinationEntity;
+
+import java.math.BigDecimal;
+
+public class TourTopViewModel {
+
+    private Long id;
+    private String name;
+    private String imageUrl;
+    private String destination;
+    private BigDecimal price;
+
+    public TourTopViewModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public TourTopViewModel setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TourTopViewModel setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public TourTopViewModel setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+        return this;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public TourTopViewModel setDestination(String destination) {
+        this.destination = destination;
+        return this;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public TourTopViewModel setPrice(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+}
