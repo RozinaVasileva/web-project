198445f5dfa94587bdf240fd79e058790dbd1985
RozinaVasileva <vasileva.rozina@gmail.com>
2021-12-20 23:48:06 +0200

    Update YachtRestController.java

--- src/main/java/bg/softuni/yacht/web/YachtRestController.java
+++ src/main/java/bg/softuni/yacht/web/YachtRestController.java
@@ -6,6 +6,7 @@ import bg.softuni.yacht.model.view.YachtViewModel;
 import bg.softuni.yacht.repository.YachtRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,4 +47,6 @@ public class YachtRestController {
                 .body(yachtCardViewModels);
 
     }
+
+
 }
